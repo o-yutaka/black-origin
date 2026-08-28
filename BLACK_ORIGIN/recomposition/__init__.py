e649b5ba269bfed847da6149dbb80f3bcba3030a
o-yutaka <56465176+o-yutaka@users.noreply.github.com>
2026-08-29 06:00:23 +0900

    feat: export recomposition core

---
+++ BLACK_ORIGIN/recomposition/__init__.py
@@ -0,0 +1,4 @@
+from BLACK_ORIGIN.recomposition.core import RecompositionCore
+from BLACK_ORIGIN.recomposition.module import RecompositionModule
+
+__all__ = ["RecompositionCore", "RecompositionModule"]
