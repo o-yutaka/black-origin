12eb4a5b08f3102396b9418f8c1ebc512f75b36c
o-yutaka <56465176+o-yutaka@users.noreply.github.com>
2026-08-29 06:10:42 +0900

    feat: export parallel evaluator types

--- BLACK_ORIGIN/recomposition/__init__.py
+++ BLACK_ORIGIN/recomposition/__init__.py
@@ -1,5 +1,19 @@
 from BLACK_ORIGIN.recomposition.core import RecompositionCore
+from BLACK_ORIGIN.recomposition.evaluation import (
+    EvaluatorAdapter,
+    FunctionEvaluator,
+    ParallelEvaluationError,
+    ParallelEvaluatorSuite,
+)
 from BLACK_ORIGIN.recomposition.materializer import ReconstructionMaterializer
 from BLACK_ORIGIN.recomposition.module import RecompositionModule
 
-__all__ = ["RecompositionCore", "ReconstructionMaterializer", "RecompositionModule"]
+__all__ = [
+    "EvaluatorAdapter",
+    "FunctionEvaluator",
+    "ParallelEvaluationError",
+    "ParallelEvaluatorSuite",
+    "RecompositionCore",
+    "ReconstructionMaterializer",
+    "RecompositionModule",
+]
